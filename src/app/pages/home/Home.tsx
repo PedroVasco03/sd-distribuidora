f2e89cb54fc03b424ad02a8c7bc4134bbc1a63bd
pedrovasco <baltazarpedro092@gmail.com>
2023-12-01 20:26:58 +0100

    nav & fonts

---
+++ src/app/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar/Navbar";
+
+const Home = () =>{
+    return(
+        <>
+            <Navbar/>  
+            
+            <h1>home page</h1>
+        </>
+    )
+}
+
+export default Home;
